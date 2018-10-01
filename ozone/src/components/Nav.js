12cad598b4913f176f3b874b1ce975a645e25a17
tedLeary <markeleary@gmail.com>
2018-10-01 10:57:25 -0700

    my list and route list links now clear modal

--- ozone/src/components/Nav.js
+++ ozone/src/components/Nav.js
@@ -3,7 +3,7 @@ import ReactDom from 'react-dom';
 import styles from './nav.css'
 import {Link} from 'react-router-dom';
 import { connect } from 'react-redux'
-import { emptyArray } from './../actions';
+import { emptyArray, detailModal } from './../actions';
 
 class List extends React.Component {
   render() {
@@ -12,9 +12,9 @@ class List extends React.Component {
     return (
       <div className={styles.navWrapper}>
         <div className={styles.nav}>
-          <Link to ="/myList"><div className={styles.navButton}>My List</div></Link>
+          <Link to ="/myList"><div onClick={()=> dispatch(detailModal(''))} className={styles.navButton}>My List</div></Link>
           <Link to ="/"><div onClick={()=> dispatch(emptyArray())}  className={styles.navButton}>Home</div></Link>
-          <Link to ="/list"><div className={styles.navButton}>Route List</div></Link>
+          <Link to ="/list"><div onClick={()=> dispatch(detailModal(''))} className={styles.navButton}>Route List</div></Link>
         </div>
       </div>
     );
